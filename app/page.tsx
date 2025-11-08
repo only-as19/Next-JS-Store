6d472aa8f8a1d3437ede397de68a0cd6ca8fa718
Asad <hellohello51981@gmail.com>
2025-11-08 21:22:03 +0500

    Feat: Add shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 const Home = () => {
   return (
     <div>
-      Home
+      <h1 className="text-xl font-bold">Home</h1>
+      <Button variant={"outline"}>Click ME</Button>
     </div>
   )
 }
