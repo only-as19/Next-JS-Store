60d40b8faf7ebd6fb562e1b09fa0f8cd9c56820f
Asad <hellohello51981@gmail.com>
2025-11-09 06:44:20 +0500

    Feat: Added Container Component

---
+++ components/global/Container.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+interface ContainerProps {
+    className:string;
+    children:ReactNode
+}
+
+const Container:React.FC<ContainerProps> = ({className,children}) => {
+  return (
+    <div className={cn('mx-auto max-w-6xl xl:max-w-7xl px-8',className)}>
+      {children}
+    </div>
+  )
+}
+
+export default Container
