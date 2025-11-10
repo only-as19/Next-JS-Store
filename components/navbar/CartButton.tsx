105d734a8da9e88872d9efb947dce395bea953a1
Asad <hellohello51981@gmail.com>
2025-11-10 21:42:40 +0500

    Feat: Complete CartButton component

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
+import { Button } from "../ui/button"
+import { LuShoppingCart } from "react-icons/lu";
+
 const CartButton = () => {
+   const cartItems = 9
   return (
-    <div>
-      CartButton
-    </div>
+    <Button variant="outline" size="icon" className="flex items-center justify-center relative">
+      <Link href="/cart">
+        <LuShoppingCart/>
+        <span className="absolute -top-3 -right-3 bg-primary text-white h-6 w-6 rounded-full flex items-center justify-center text-xs">{cartItems}</span>
+      </Link>
+    </Button>
   )
 }
 
