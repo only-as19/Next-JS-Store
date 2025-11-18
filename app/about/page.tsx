1f44c9bcfbd262d37749a340e2875502a7c20c94
Asad <hellohello51981@gmail.com>
2025-11-18 21:20:18 +0500

    feat: Practice some queries

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,17 @@
-const AboutPage = () => {
+import db from "@/utils/db"
+const AboutPage = async () => {
+
+  const profile = await db.testProfile.create({
+    data:{
+      name: "Asad"
+    }
+  })
+
+  const users = await db.testProfile.findMany()
+  console.log(users)
   return (
     <div>
-      AboutPage
+      
     </div>
   )
 }
